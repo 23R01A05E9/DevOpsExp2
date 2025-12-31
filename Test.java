8c07d0219f71842013a154d3e2799bd462b1b900
student <student@host.docker.internal>
2025-12-31 11:03:18 +0530

    changes

---
+++ Test.java
@@ -0,0 +1,8 @@
+import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("demo for exp2");
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2><%= "Hello World!" %></h2>
+<h2><%= "wrote my ciontent to see the changes in  git"%></h2>
 </body>
 </html>
